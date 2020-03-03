38cd35dc44e9607d4f4e7b55c7b2dff36a9578a4
Sergei Imagine <acrashik@gmail.com>
2020-03-03 15:43:48 +0200

    relative path fix

--- config.php
+++ config.php
@@ -5,12 +5,12 @@
 	define('APP_DESCRIPTION','Send your own transfers with love');
 
 	define('APP_VIDEO', false); //APP_URL.'img/appbg.mp4'
-	define('APP_LOGO', APP_URL.'img/logo.png');
+	define('APP_LOGO', APP_URL . 'img/logo.png');
 
 	define('APP_SHORTENER', 'https://t.imagine.ee/'); /* optional for nice looking URLs */
 
 	define('KEEP_DAYS', 7); /* How many days till transfer will be removed */
-	define('SERVER_URL','/server/');
+	define('SERVER_URL', APP_URL . 'server/');
 	define('FILES_PATH','/server/files/');
 
 
